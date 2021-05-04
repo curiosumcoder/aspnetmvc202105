21e78f730b1651a01677b2b06320d8c9c619c9e5
Gilberto Bermúdez Garro <gbermude@outlook.com>
2021-05-04 12:10:11 -0600

    Ejemplo del día

---
+++ 20210504/CA1/CA1/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using MEP.Model;
+
+namespace CA1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            MEP.Model.Estudiante e1 = new MEP.Model.Estudiante();
+            e1.Id = 123;
+            e1.Nombre = "Juan";
+            Console.WriteLine($"{e1.Id} {e1.Nombre}");
+
+            Estudiante e2 = new Estudiante() { Id = 456, Nombre = "María" };
+            Console.WriteLine($"{e2.Id} - {e2.Nombre}");
+
+            Console.WriteLine("Hello World!");
+            Console.ReadLine();
+
+            //var p = new CA1.Program();
+        }
+    }
+}
---
+++ 20210504/CA1/MEP.Model/Estudiante.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MEP.Model
+{
+    public class Estudiante
+    {
+        // Variable
+        // public string Apellido;
+
+        // Propiedad
+        public int Id { get; set; }
+        public string Nombre { get; set; }
+        public string Apellido1 { get; set; }
+        public string Apellido2 { get; set; }
+    }
+}
