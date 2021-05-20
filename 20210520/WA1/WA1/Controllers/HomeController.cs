324bd2dd2a9117b2fae6987c0a9379a62e0ec9dd
Gilberto Bermúdez Garro <gbermude@outlook.com>
2021-05-20 09:40:09 -0600

    Demostración del día

---
+++ 20210520/WA1/WA1/Controllers/HomeController.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using WA1.Models;
+
+namespace WA1.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index(string filtro = "")
+        {
+            List<Product> ps = new List<Product>();
+
+            using (var db = new NorthwindContext())
+            {
+                ps = db.Products.Where(p => p.ProductName.Contains(filtro)).ToList();
+            }
+
+            ViewBag.filtro = filtro;
+
+            ViewData["products"] = ps;
+            ViewBag.products = ps;
+
+            return View();
+        }
+
+        public ActionResult About()
+        {
+            ViewBag.Message = "Your application description page.";
+
+            return View();
+        }
+
+        public ActionResult Contact()
+        {
+            ViewBag.Message = "Your contact page.";
+
+            return View();
+        }
+    }
+}
